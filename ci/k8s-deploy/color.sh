ad1a58d6e041f3e8baa646332410135b8790d405
ChenLong Ma <owlet42@126.com>
2021-01-08 13:04:27 +0800

    fix color
    Signed-off-by: ChenLong Ma <owlet42@126.com>

--- ci/k8s-deploy/color.sh
+++ ci/k8s-deploy/color.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-INFO="\033[36mINFO:\033[0m"
+INFO="\033[36mInfo:\033[0m"
 Warning="\033[33mWarning:\033[0m"
-ERROR="\033[31mERROR:\033[0m"
-Success="\033[32mSuccess:\033[0m"
-DEBUG="\033[34mSuccess:\033[0m"
+ERROR="\033[31mErrot:\033[0m"
+SUCCESS="\033[32mSuccess:\033[0m"
+DEBUG="\033[34mDebug:\033[0m"
 
 log() {
     echo $@
